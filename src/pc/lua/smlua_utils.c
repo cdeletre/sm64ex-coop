d533747b7e789330ce6032b21470f3096b62e1d6
Robert Kirkman <misternumberone@live.com>
2023-11-26 14:03:38 -0600

    enable smlua_push_object() fix for all distros

--- src/pc/lua/smlua_utils.c
+++ src/pc/lua/smlua_utils.c
@@ -441,21 +441,21 @@ void smlua_push_object(lua_State* L, u16 lot, void* p) {
     }
 
     // add to allowlist
-#ifdef __ANDROID__
+// #ifdef __ANDROID__
     smlua_cobject_allowlist_add(lot, (u64)(intptr_t)p);
-#else // yep this destroys 32-bit ARM bionic support
-    u64 pointer = (uintptr_t) p;
-    smlua_cobject_allowlist_add(lot, pointer);
-#endif
+// #else // yep this destroys 32-bit ARM support for bionic and musl at the very least
+//     u64 pointer = (uintptr_t) p;
+//     smlua_cobject_allowlist_add(lot, pointer);
+// #endif
 
     // get a cobject from a function
     lua_getglobal(L, "_NewCObject");  // Get the function by its global name
     lua_pushinteger(L, lot);
-#ifdef __ANDROID__
+// #ifdef __ANDROID__
     lua_pushinteger(L, (u64)(intptr_t)p);
-#else
-    lua_pushinteger(L, pointer);
-#endif
+// #else
+//     lua_pushinteger(L, pointer);
+// #endif
 
     if (lua_pcall(L, 2, 1, 0) != LUA_OK) {
         LOG_ERROR("Error calling Lua function: %s\n", lua_tostring(L, -1));
@@ -468,21 +468,21 @@ void smlua_push_pointer(lua_State* L, u16 lvt, void* p) {
         return;
     }
 
-#ifdef __ANDROID__
+// #ifdef __ANDROID__
     smlua_cpointer_allowlist_add(lvt, (u64)(intptr_t)p);
-#else
-    u64 pointer = (uintptr_t) p;
-    smlua_cpointer_allowlist_add(lvt, pointer);
-#endif
+// #else
+//     u64 pointer = (uintptr_t) p;
+//     smlua_cpointer_allowlist_add(lvt, pointer);
+// #endif
 
     // get a cpointer from a function
     lua_getglobal(L, "_NewCPointer");  // Get the function by its global name
     lua_pushinteger(L, lvt);
-#ifdef __ANDROID__
+// #ifdef __ANDROID__
     lua_pushinteger(L, (u64)(intptr_t)p);
-#else
-    lua_pushinteger(L, pointer);
-#endif
+// #else
+//     lua_pushinteger(L, pointer);
+// #endif
     if (lua_pcall(L, 2, 1, 0) != LUA_OK) {
         LOG_ERROR("Error calling Lua function: %s\n", lua_tostring(L, -1));
     }
